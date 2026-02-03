dbd105bfe776487e97df114cf6ee5022b2771339
agabrielcorujo <agabrielcorujo@gmail.com>
2026-02-03 17:33:03 -0500

    adding initial scaffolding for application and presentation layers

--- backend/src/business_logic.py
+++ backend/src/business_logic.py
@@ -1,2 +1,12 @@
-from jwt_auth.db import safe_query
+from jwt_auth.db import safe_query #syntax: result = safe_query(query,(param1,param2...),fetch="one/all",insert=True/False)
+import boto3 as aws
+from dotenv import load_dotenv
+
+class AppError(Exception):
+    def __init__(self, message: str, status_code: int = 400):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(message)
+
+
 
--- backend/src/business_router.py
+++ backend/src/business_router.py
@@ -1,18 +1,38 @@
-from fastapi import Depends,FastAPI,APIRouter, HTTPException
+from fastapi import Depends,APIRouter, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel 
+from typing import Optional
 from jwt_auth.jwt_auth import decode_access_token
-import business_logic
+from business_logic import (
+    AppError
+)
 
 router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
+class SampleRequest(BaseModel):
+    request_param1: str #required parameter
+    request_param2: Optional[str] = None #not required, optional parameter
+
+
 @router.get("/sample-endpoint")
-def sample_endpoint(token:str = Depends(oauth2_scheme)):
+def sample_endpoint(Request:SampleRequest,token:str = Depends(oauth2_scheme)):
 
-    user_id = decode_access_token(token)
+    user_id = decode_access_token(token) #this automatically raises 401 unauthorized if the token is invalid
 
-    #do something by calling functions in business_logic.py
-    
-    pass
+    try:
+        #do something by calling functions in business_logic.py
+        variable1 = Request.request_param1
+        variable2 = Request.request_param2
+        ...
+
+    except AppError as Err:
+
+        raise HTTPException(
+            status_code=Err.status_code,
+            detail=Err.message
+        )
+
+    return {"response":"some response"}
 
