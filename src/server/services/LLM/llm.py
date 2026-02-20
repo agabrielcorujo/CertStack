2e382c9958b6f2273abe2845cddbb5c0239b28a4
agabrielcorujo <agabrielcorujo@gmail.com>
2026-02-20 09:23:49 -0500

    separated llm scraping and chunking from querying

--- src/server/services/LLM/llm.py
+++ src/server/services/LLM/llm.py
@@ -1,17 +1,8 @@
-from langchain_community.document_loaders import TextLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_community.vectorstores import UpstashVectorStore
 from langchain_openai import OpenAIEmbeddings
 import os
-from dotenv import load_dotenv
-
-
-#only call setup() with local data, not in docker. pls dont load data into docker container
-
-
-DATA_PATH = "some path to data to be chunked"
 
 PROMPT_TEMPLATE="""
 Answer the question based only on the following context:
@@ -33,40 +24,6 @@ ENVS = {
 if not all(ENVS.values()):
 
     raise RuntimeError("LLM configuration error")
-
-def load_docs():
-    documents = []
-    for filename in os.listdir(DATA_PATH):
-        if filename.endswith(".txt"):
-            loader = TextLoader(os.path.join(DATA_PATH, filename))
-            documents.extend(loader.load())
-    return documents
-
-def split_text(documents:list):
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=100,
-        length_function=len,
-        add_start_index=True,
-        separators = [
-            "\n          ",
-            "\n\n",
-            "\n",
-        ],
-    )
-
-    chunks = text_splitter.split_documents(documents)
-
-    return chunks
-
-def save_to_upstash(chunks):
-    vector_store = UpstashVectorStore.from_documents(
-        documents=chunks,
-        embedding=OpenAIEmbeddings(),
-        index_url=os.getenv("UPSTASH_VECTOR_REST_URL"),
-        index_token=os.getenv("UPSTASH_VECTOR_REST_TOKEN"),
-    )
 
 def query_db(query: str):
 
@@ -94,10 +51,6 @@ def query_db(query: str):
     })
 
     print(response.content)
-
-def setup():
-    chunks = split_text(load_docs())
-    save_to_upstash(chunks)
 
 if __name__ == "__main__":
 
