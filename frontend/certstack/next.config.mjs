7c71667e1704c8ce1ec7639104f3e132ab3fccab
Verónica Urgell del Prado <urgelv@rpi.edu>
2026-04-06 22:53:40 -0400

    chore(frontend)

--- frontend/certstack/next.config.mjs
+++ frontend/certstack/next.config.mjs
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-export default nextConfig;
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
